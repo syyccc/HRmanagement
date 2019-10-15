d4bd0f601828033a2f9ea0f0c8d0d94cd9fd0640
zoe <shi99@wisc.edu>
2019-10-15 15:08:11 -0700

    first commit

---
+++ HRserver/src/main/java/com/ss/HRserver/HRserverApplication.java
@@ -0,0 +1,13 @@
+package com.ss.HRserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRserverApplication.class, args);
+	}
+
+}
